1a141484e0cc9ea2d38e81ff0db5c5795b810c5a
Vasvári Gergő <gergo.vasvari@gmail.com>
2026-05-17 16:51:05 +0200

    [exercise 2] add backup retention lifecycle

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -78,3 +78,28 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backups" {
     bucket_key_enabled = true
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "backups" {
+  bucket = aws_s3_bucket.backups.id
+
+  rule {
+    id     = "expire-backups-after-retention-period"
+    status = "Enabled"
+
+    filter {}
+
+    expiration {
+      days = var.retention_days
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = var.retention_days
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+
+  depends_on = [aws_s3_bucket_versioning.backups]
+}
