0f4814d362a0eb65cf26a269fa6898857e747077
Vasvári Gergő <gergo.vasvari@gmail.com>
2026-05-17 14:50:09 +0200

    [exercise 1] add public and private internet routing

--- exercise_1/modules/vpc/main.tf
+++ exercise_1/modules/vpc/main.tf
@@ -43,3 +43,74 @@ resource "aws_subnet" "private" {
     Tier = "private"
   })
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-igw"
+  })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-public"
+    Tier = "public"
+  })
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-nat"
+  })
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-nat"
+  })
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_route_table" "private" {
+  count = 2
+
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-private-${count.index + 1}"
+    Tier = "private"
+  })
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
--- exercise_1/modules/vpc/outputs.tf
+++ exercise_1/modules/vpc/outputs.tf
@@ -12,3 +12,13 @@ output "private_subnet_ids" {
   description = "IDs of private subnets."
   value       = aws_subnet.private[*].id
 }
+
+output "public_route_table_id" {
+  description = "ID of the public route table."
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "IDs of private route tables."
+  value       = aws_route_table.private[*].id
+}
